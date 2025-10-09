af0fd4b37139990f9ab821090e3364ed8d5a052d
Thanh <thanhb2207565@student.ctu.edu.vn>
2025-10-09 15:32:58 +0700

    done pdftotext.pdf

--- src/pdftotext.py
+++ src/pdftotext.py
@@ -1,2 +1,17 @@
 # code here
 
+import fitz  
+
+doc = fitz.open("") # Open a PDF file
+
+extracted_text = ""
+
+for page in doc:
+    extracted_text += page.get_text() + "\n"
+
+print(extracted_text)
+
+with open("output.txt", "w", encoding="utf-8") as f:
+    f.write(extracted_text)
+
+doc.close()
